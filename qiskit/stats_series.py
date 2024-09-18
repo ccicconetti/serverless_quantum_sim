eae7b672958b59178d4aa83134db28a640ac945f
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2024-09-18 11:29:24 +0000

    Qiskit: update stats_series.py

--- qiskit/stats_series.py
+++ qiskit/stats_series.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 import os
+import matplotlib.pyplot as plt
 
 filename = os.environ.get("DATASET", "output_series.csv")
 pd.set_option("display.show_dimensions", False)
@@ -10,3 +11,9 @@ df = pd.read_csv(filename)
 metrics = ["exec_time", "cost_time"]
 for metric in metrics:
     print("{}\n{}".format(metric, df.groupby(["n_qubits"])[metric].describe()))
+    
+axes = df.boxplot(column=["exec_time", "cost_time"],by=["n_qubits"])
+for ax in axes:
+    ax.set_title("")
+    ax.set_ylabel("Time (s)")
+plt.show()
