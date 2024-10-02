fad30e190ba882981c73c482fe4cbc225a12bdca
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2024-10-02 14:29:17 +0200

    Qiskit update plot scripts

--- qiskit/stats_series.py
+++ qiskit/stats_series.py
@@ -3,17 +3,45 @@
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-filename = os.environ.get("DATASET", "output_series.csv")
-pd.set_option("display.show_dimensions", False)
-df = pd.read_csv(filename)
+IMAGE_TYPE = os.environ.get("IMAGE_TYPE", "png")
+DATASET = os.environ.get("DATASET", "output_series.csv")
+SHOW = bool(os.environ.get("SHOW", ""))
 
-metrics = ["exec_time", "cost_time"]
-for metric in metrics:
-    print("{}\n{}".format(metric, df.groupby(["n_qubits"])[metric].describe()))
-    
-axes = df.boxplot(column=["exec_time", "cost_time"],by=["n_qubits"])
-for ax in axes:
+
+def plot(
+    df, x: str, y: str, ylabel: str | None, hue: str | None, show: bool, filename: str
+):
+    fig, ax = plt.subplots()
+    sns.boxplot(df, x=x, y=y, hue=hue, ax=ax)
     ax.set_title("")
-    ax.set_ylabel("Time (s)")
-plt.show()
+    ax.set_ylabel(ylabel)
+    fig.suptitle("")
+    if show:
+        plt.show(block=False)
+    else:
+        plt.savefig("{}.{}".format(filename, IMAGE_TYPE))
+
+
+pd.set_option("display.show_dimensions", False)
+df = pd.read_csv(DATASET)
+
+metrics = [
+    ("exec_time", "Quantum task execution time (ms)"),
+    ("cost_time", "Classical task execution time (ms)"),
+]
+for ymetric, ylabel in metrics:
+    df[ymetric] = df[ymetric] * 1000.0
+    plot(
+        df,
+        x="n_qubits",
+        y=ymetric,
+        ylabel=ylabel,
+        hue=None,
+        show=SHOW,
+        filename="{}-{}".format(os.path.basename(os.getcwd()), ymetric),
+    )
+
+if SHOW:
+    input("Press any key to continue")
--- qiskit/stats_single.py
+++ qiskit/stats_single.py
@@ -3,36 +3,52 @@
 import pandas as pd
 import os
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-filename = os.environ.get("DATASET", "output_single.csv")
+IMAGE_TYPE = os.environ.get("IMAGE_TYPE", "png")
+DATASET = os.environ.get("DATASET", "output_single.csv")
+SHOW = bool(os.environ.get("SHOW", ""))
+
+
+def plot(
+    df, x: str, y: str, ylabel: str | None, hue: str | None, show: bool, filename: str
+):
+    fig, ax = plt.subplots()
+    sns.boxplot(df, x=x, y=y, hue=hue, ax=ax)
+    ax.set_title("")
+    ax.set_ylabel(ylabel)
+    fig.suptitle("")
+    if show:
+        plt.show(block=False)
+    else:
+        plt.savefig("{}.{}".format(filename, IMAGE_TYPE))
+
+
 pd.set_option("display.show_dimensions", False)
 pd.set_option("display.max_columns", None)
 pd.set_option("display.max_rows", None)
 pd.set_option("display.max_colwidth", None)
-df = pd.read_csv(filename, index_col=False)
+df = pd.read_csv(DATASET, index_col=False)
 df = df.select_dtypes(include="number")
 print(df.describe())
 
-columns = ["QUEUED", "INITIALIZING", "RUNNING", "num_iterations"]
+metrics = [
+    ("QUEUED", "Time (s)"),
+    ("INITIALIZING", "Time (s)"),
+    ("RUNNING", "Time (s)"),
+    ("num_iterations", "Number of iterations"),
+]
 
-if os.environ.get("SAVE", "") == "":
-    fig, ax = plt.subplots(1, len(columns), figsize=(20, 5))
-    for i, column in zip(range(len(columns)), columns):
-        if column == "num_iterations":
-            ax[i].set_ylabel("Number of iterations")
-        else:
-            ax[i].set_ylabel("Time (s)")
-        df.boxplot(column=[column], by=["n_qubits"], ax=ax[i])
-    fig.suptitle("")
-    plt.show()
+for ymetric, ylabel in metrics:
+    plot(
+        df,
+        x="n_qubits",
+        y=ymetric,
+        ylabel=ymetric,
+        hue=None,
+        show=SHOW,
+        filename="{}-{}".format(os.path.basename(os.getcwd()), ymetric),
+    )
 
-else:
-    for i, column in zip(range(len(columns)), columns):
-        fig, ax = plt.subplots()
-        if column == "num_iterations":
-            ax.set_ylabel("Number of iterations")
-        else:
-            ax.set_ylabel("Time (s)")
-        df.boxplot(column=[column], by=["n_qubits"], ax=ax)
-        fig.suptitle("")
-        fig.savefig("{}.png".format(column))
+if SHOW:
+    input("Press any key to continue")
