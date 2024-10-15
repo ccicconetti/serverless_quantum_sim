36f850604dba3c585c2fd6f9f33f08f6fcca1f56
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2024-10-15 11:15:39 +0200

    Qiskit: update script to create simulation dataset

--- qiskit/convert_output.py
+++ qiskit/convert_output.py
@@ -7,16 +7,21 @@ filename_single = os.environ.get("DATASET_SINGLE", "output_single.csv")
 df = pd.read_csv(filename_single, index_col=False)
 df = df.select_dtypes(include="number")
 
-df["pre"] = df["QUEUED"] + df["INITIALIZING"]
+df["pre"] = df["QUEUED"] + df["INITIALIZING"] + df["run_transpile"] + df["run_init"]
+df.rename(
+    columns={"avg_clas_iter_dur": "cost_time", "run_sampler": "post"}, inplace=True
+)
+print(df)
 
-metrics = ["pre", "run_sampler", "num_iterations"]
+metrics = ["pre", "post", "num_iterations", "cost_time"]
 for metric in metrics:
     df.to_csv("{}.csv".format(metric), columns=["n_qubits", metric], index=False)
 
 filename_series = os.environ.get("DATASET_SERIES", "output_series.csv")
 df = pd.read_csv(filename_series, index_col=False)
 df = df.select_dtypes(include="number")
+df.rename(columns={"time": "exec_time"}, inplace=True)
 
-metrics = ["cost_time", "exec_time"]
+metrics = ["exec_time"]
 for metric in metrics:
     df.to_csv("{}.csv".format(metric), columns=["n_qubits", metric], index=False)
