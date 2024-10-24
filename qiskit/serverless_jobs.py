4861c5d426c615863155cfa915d47d68f25a23e9
Claudio Cicconetti <c.cicconetti@iit.cnr.it>
2024-10-24 10:46:42 +0200

    Add option to retrieve a single serverless job result or logs

--- qiskit/serverless_jobs.py
+++ qiskit/serverless_jobs.py
@@ -1,11 +1,25 @@
 # SPDX-FileCopyrightText: © 2024 Claudio Cicconetti <c.cicconetti@iit.cnr.it>
 # SPDX-License-Identifier: MIT
 
+import os
+
 from qiskit_serverless import IBMServerlessClient
 from ibm_credentials import get_ibm_credentials
 
 ibm_credentials = get_ibm_credentials()
 
+LOGS = bool(os.environ.get("LOGS", ""))
+JOB = os.environ.get("JOB", "")
+
 serverless = IBMServerlessClient(token=ibm_credentials["TOKEN"])
-for job in serverless.get_jobs():
-    print("job_id {}, status {}".format(job.job_id, job.status()))
+
+if JOB == "":
+    for job in serverless.get_jobs():
+        print("job_id {}, status {}".format(job.job_id, job.status()))
+else:
+    job = serverless.get_job_by_id(JOB)
+    if LOGS:
+        print(job.logs())
+    else:
+        if job.in_terminal_state():
+            print(job.result(wait=False))
